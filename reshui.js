977cc4d2316d9fe85bfea1c89e8dc8090d5ea57c
SheepFJ <125231595+SheepFJ@users.noreply.github.com>
2024-09-01 12:35:11 +0800

    Update reshui.js

--- reshui.js
+++ reshui.js
@@ -1,11 +1,19 @@
+/*************************************
+项目名称：一合物联去广告
+下载地址：
+更新日期：2024-08-21
+脚本作者：Sheep
+使用声明：⚠️仅供参考，🈲转载与售卖！
+脚本说明；热水去广告
 
+**************************************
 
+[rewrite_local]
+^^https://www\.szyhznkj\.net/xcxInterface/xcxapi/v3GetAccByWxId url script-response-body https://raw.githubusercontent.com/SheepFJ/Sheep/main/reshui.js  
+[mitm]
+hostname = www.szyhznkj.net
 
-/*
-^https://www\.fupanhezi\.com/usercenter/v1/user/detail
-
-www.fupanhezi.com
-*/
+*************************************/
 
 let body = JSON.parse($response.body);
 
