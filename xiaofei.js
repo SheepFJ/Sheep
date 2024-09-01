a09e14922deb15b2542c33b765dc5c394d027961
SheepFJ <125231595+SheepFJ@users.noreply.github.com>
2024-09-01 11:42:57 +0800

    Add files via upload

---
+++ xiaofei.js
@@ -0,0 +1,20 @@
+
+/*************************************
+项目名称：一合物联
+下载地址：http://985.so/k81ah
+更新日期：2024-09-01
+脚本作者：Sheep
+使用声明：⚠️仅供参考，🈲转载与售卖！
+脚本说明：热水
+
+**************************************
+
+[rewrite_local]
+^https://www\.szyhznkj\.net/xcxInterface/xcxapi/webXFUpSaveOrderNew url script-request-body https://raw.githubusercontent.com/SheepFJ/Sheep/main/zhiliaozhuanshengben1.js  
+[mitm]
+hostname = www.szyhznkj.net
+
+*************************************/
+
+
+const _0x56768a=_0x29e2;(function(_0x52f3d3,_0x2f3efc){const _0x5cec31=_0x29e2,_0x265fcb=_0x52f3d3();while(!![]){try{const _0x225038=-parseInt(_0x5cec31(0x19c))/0x1+-parseInt(_0x5cec31(0x1a1))/0x2*(-parseInt(_0x5cec31(0x19d))/0x3)+parseInt(_0x5cec31(0x19f))/0x4*(-parseInt(_0x5cec31(0x197))/0x5)+-parseInt(_0x5cec31(0x198))/0x6*(-parseInt(_0x5cec31(0x1a4))/0x7)+-parseInt(_0x5cec31(0x19e))/0x8*(parseInt(_0x5cec31(0x199))/0x9)+parseInt(_0x5cec31(0x1a5))/0xa+-parseInt(_0x5cec31(0x19a))/0xb*(-parseInt(_0x5cec31(0x1a2))/0xc);if(_0x225038===_0x2f3efc)break;else _0x265fcb['push'](_0x265fcb['shift']());}catch(_0x35ed1e){_0x265fcb['push'](_0x265fcb['shift']());}}}(_0x394c,0x99b37));function _0x394c(){const _0x2eaa83=['11gPbWkx','replace','1018646ZUsWiE','257154qCBGkD','88608NaGAmh','628IBCixb','body','24lHqqYp','14402352vInppx','XFLEADMONEY=4960','225365kyUhSd','9039910xobOFx','36115vnDSCk','42ZamEgP','468UlOvcq'];_0x394c=function(){return _0x2eaa83;};return _0x394c();}let body=$request[_0x56768a(0x1a0)];function _0x29e2(_0x556449,_0x55b1c7){const _0x394c8c=_0x394c();return _0x29e2=function(_0x29e271,_0xff7aa8){_0x29e271=_0x29e271-0x197;let _0x3c1ff1=_0x394c8c[_0x29e271];return _0x3c1ff1;},_0x29e2(_0x556449,_0x55b1c7);}body=body[_0x56768a(0x19b)](/XFLEADMONEY=\d+/g,_0x56768a(0x1a3)),$done({'body':body});
