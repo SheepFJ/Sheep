5c032f54df2002071a39b4121547b9bbe0bfbae4
SheepFJ <125231595+SheepFJ@users.noreply.github.com>
2024-10-12 10:21:17 +0800

    更新 chegongjisuan.js

---
+++ chegongjisuan.js
@@ -0,0 +1,16 @@
+/*************************************
+项目名称：车工计算
+下载地址：http://985.so/k8iqa
+更新日期：2024-10-12
+脚本作者：Sheep
+使用声明：⚠️仅供参考，🈲转载与售卖！
+脚本说明；解锁vip
+
+**************************************
+
+[rewrite_local]
+^http://wu18\.cn/cg/zhanghao/ url script-response-body https://raw.githubusercontent.com/SheepFJ/Sheep/main/chegongjisuan.js  
+[mitm]
+hostname = wu18.cn
+
+*************************************/
