74fc85d3661530d871fe4d44296e47810b40767e
SheepFJ <125231595+SheepFJ@users.noreply.github.com>
2024-11-15 19:59:25 +0800

    Update xiaomishuabushu.js

--- sheepTask/sheep_rewrite_local/xiaomishuabushu.js
+++ sheepTask/sheep_rewrite_local/xiaomishuabushu.js
@@ -13,7 +13,7 @@ if (url.includes(targetPath)) {
     const minStepsSuccess = $prefs.setValueForKey(minSteps, "sheep_min_steps_xiaomishuabushu");
     const maxStepsSaveSuccess = $prefs.setValueForKey(maxSteps, "sheep_max_steps_xiaomishuabushu");
     if (accountSaveSuccess && passwordSaveSuccess && minStepsSuccess && maxStepsSaveSuccess ) {
-        $notify("账号和密码本地存储成功","","账号: ${account},密码: ${password},步数范围: ${minSteps}--${maxSteps}");
+        $notify("账号和密码本地存储成功","",`账号: ${account},密码: ${password},步数范围: ${minSteps}--${maxSteps}`);
     } else {
         $notify("账号和密码保存失败",`1.是否引用重写 2.传参链接是否规范`);
     }
