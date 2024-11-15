a01ffa74254322ec17530f85ccebd66a6da557a7
SheepFJ <125231595+SheepFJ@users.noreply.github.com>
2024-11-15 20:36:40 +0800

    Update xiaomishuabushu.js

--- sheepTask/xiaomishuabushu.js
+++ sheepTask/xiaomishuabushu.js
@@ -2,7 +2,7 @@
 /*
 ❕使用教程
 1.下载Zeep Life,app里面--我的👉第三方接入👉绑定微信或者支付宝
-2.记住账号密码与要刷的步数范围填写到下方链接后在浏览器运行一次"    https://api.sheeptask.com/xiaomishuabushu/你的账号/你的密码/最小步数/最大步数/    "
+2.记住账号密码与要刷的步数范围填写到下方链接后在浏览器运行一次即可"    https://api.sheeptask.com/xiaomishuabushu/你的账号/你的密码/最小步数/最大步数/    "
 */
 
 const account = encodeURIComponent($prefs.valueForKey("sheep_account_xiaomishuabushu") || "");
@@ -35,6 +35,6 @@ $task.fetch(myRequest).then(response => {
     $done();
 }, reason => {
     // 请求出错时显示通知
-    $notify("步数提交失败","", `请求出错错误信息: ${reason.error}`);
+    $notify("步数提交失败","", `请求出错，可能需要重新运行：https://api.sheeptask.com/xiaomishuabushu/你的账号/你的密码/最小步数/最大步数/`);
     $done();
 });
