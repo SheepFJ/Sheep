08947f3422ab293b4255077c13851d52c2263ad8
SheepFJ <125231595+SheepFJ@users.noreply.github.com>
2024-12-13 22:54:19 +0800

    更新 cadmini.js

---
+++ cadmini.js
@@ -0,0 +1,15 @@
+/*************************************
+项目名称：CAD迷你看图
+更新日期：2024-09-22
+脚本作者：Sheep
+使用声明：⚠️仅供参考，🈲转载与售卖！
+脚本说明：不限制使用次数,次数上限后重新加载即可
+
+**********************************
+
+[rewrite_local]
+^http://app\.pcw365\.com/ios/service_use url script-response-body https://raw.githubusercontent.com/SheepFJ/Sheep/main/cadmini.js  
+[mitm]
+hostname = app.pcw365.com
+*************************************/
+var _0x242d84=_0x5048;(function(_0x520abc,_0x281922){var _0x54db46=_0x5048,_0x3d9fdf=_0x520abc();while(!![]){try{var _0x5cf80d=-parseInt(_0x54db46(0x71))/0x1+-parseInt(_0x54db46(0x74))/0x2*(-parseInt(_0x54db46(0x7a))/0x3)+-parseInt(_0x54db46(0x73))/0x4+-parseInt(_0x54db46(0x75))/0x5+-parseInt(_0x54db46(0x77))/0x6*(-parseInt(_0x54db46(0x79))/0x7)+parseInt(_0x54db46(0x7b))/0x8*(parseInt(_0x54db46(0x70))/0x9)+-parseInt(_0x54db46(0x72))/0xa*(-parseInt(_0x54db46(0x78))/0xb);if(_0x5cf80d===_0x281922)break;else _0x3d9fdf['push'](_0x3d9fdf['shift']());}catch(_0x156247){_0x3d9fdf['push'](_0x3d9fdf['shift']());}}}(_0x54b6,0x353f2));function _0x5048(_0x471048,_0x48c858){var _0x54b660=_0x54b6();return _0x5048=function(_0x5048ef,_0x905944){_0x5048ef=_0x5048ef-0x6e;var _0x3bc1aa=_0x54b660[_0x5048ef];return _0x3bc1aa;},_0x5048(_0x471048,_0x48c858);}var body=$response[_0x242d84(0x6f)],obj=JSON[_0x242d84(0x76)](body);function _0x54b6(){var _0xc67e69=['270KMlIwU','153312YdYnon','stringify','body','54dUwNmf','309819tAOKSg','10AiQZRG','609080yNeTZm','6926bUPPKc','432775YjXPzh','parse','18Qmfsnh','197857KUSyPi','751569oCOJsK'];_0x54b6=function(){return _0xc67e69;};return _0x54b6();}obj={'service':'cadstopdfs','times':'0'},body=JSON[_0x242d84(0x6e)](obj),$done({'body':body});
