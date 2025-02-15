78390776159e7077c1158b9a1caaab40a2cb4f9f
SheepFJ <125231595+SheepFJ@users.noreply.github.com>
2025-02-15 13:01:59 +0800

    更新 newsPkcImage.js

--- jianxun/newsPkcImage.js
+++ jianxun/newsPkcImage.js
@@ -1,1 +1,15 @@
-1
+/*************************************
+项目名称：每日新闻60s图片PKC接口
+更新日期：2025-02-12
+脚本作者：Sheep
+使用声明：⚠️仅供参考，🈲转载与售卖！
+脚本说明：PKC微信API接口
+
+**************************************
+
+[rewrite_local]
+^https:\/\/movies\.disney\.com\/sheep\/news\/image\/? url script-response-body https://raw.githubusercontent.com/SheepFJ/Sheep/refs/heads/main/jianxun/newsPkcImage.js
+[mitm]
+hostname = movies.disney.com
+
+*************************************/
