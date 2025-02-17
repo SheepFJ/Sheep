e34dab8970e8b5c170592ae0c1f3a1c561262d9a
SheepFJ <125231595+SheepFJ@users.noreply.github.com>
2025-02-17 20:07:21 +0800

    更新 console.js

--- jianxun/console.js
+++ jianxun/console.js
@@ -1,5 +1,25 @@
-console.log("001");  // 输出字符串 "001"
-$done({
-  status: "ok",       // 自定义状态
-  message: "Execution successful"  // 自定义消息
-});
+const urldata="https://raw.githubusercontent.com/SheepFJ/Sheep/refs/heads/main/sheepWeChatPKC/sheepwechatpkc.js";
+
+const tagname="sheepPkcWeChat";
+
+// 需要编码的 JSON 对象
+const jsonObject = {
+  "rewrite_remote": [
+    urldata+", tag="+tagname+", update-interval=172800, opt-parser=true, enabled=true"
+  ]
+};
+
+// 将 JSON 对象转为字符串
+const jsonString = JSON.stringify(jsonObject);
+
+// 对整个 JSON 字符串进行 URL 编码
+const encodedJson = encodeURIComponent(jsonString);
+
+// 拼接最终的 URL
+const finalUrl = "https://quantumult.app/x/open-app/add-resource?remote-resource=" + encodedJson;
+
+// 输出最终的 URL
+console.log(finalUrl);
+
+$notify("编码后的快速导入链接","", finalUrl);
+$done();
